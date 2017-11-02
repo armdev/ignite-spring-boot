6ce15f2d11fcdee2eb51d791c59cd8760b84e628
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-11-02 18:56:17 +0400

    adding new config

--- src/main/java/io/project/app/IgniteConfig.java
+++ src/main/java/io/project/app/IgniteConfig.java
@@ -7,6 +7,8 @@ import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
+import org.apache.ignite.cache.CacheMode;
 
 /**
  *
@@ -22,13 +24,22 @@ public class IgniteConfig {
         cfg.setIgniteInstanceName("springDataNode");
         // Enabling peer-class loading feature.
         cfg.setPeerClassLoadingEnabled(true);
+        
+     
+       
         // Defining and creating a new cache to be used by Ignite Spring Data
         // repository.
         CacheConfiguration ccfg = new CacheConfiguration("FlightCache");
+        ccfg.setAtomicityMode(TRANSACTIONAL);
+        
+         ccfg.setCacheMode(CacheMode.REPLICATED);
+
         // Setting SQL schema for the cache.
         ccfg.setIndexedTypes(Long.class, Flight.class);          
         cfg.setActiveOnStart(true);    
-        cfg.setCacheConfiguration(ccfg);
+        cfg.setCacheConfiguration(ccfg);       
         return Ignition.start(cfg);
     }
+    
+    // https://github.com/apache/ignite/blob/master/examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryExample.java
 }
