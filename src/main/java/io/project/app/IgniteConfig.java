9601c8b8e5f0261975b64c935226a3c6dfba8925
armdev <armen.arzumanyan@gmail.com>
2017-11-03 01:35:29 +0400

    new updates

--- src/main/java/io/project/app/IgniteConfig.java
+++ src/main/java/io/project/app/IgniteConfig.java
@@ -1,14 +1,20 @@
 package io.project.app;
 
 import io.project.model.Flight;
+import io.project.repositories.FlightRepository;
+import javax.cache.configuration.FactoryBuilder;
 import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import static org.apache.ignite.Ignition.ignite;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static org.apache.ignite.cache.CacheAtomicityMode.TRANSACTIONAL;
 import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.spring.SpringCacheManager;
+import org.apache.ignite.cache.store.CacheStore;
 
 /**
  *
@@ -18,28 +24,38 @@ import org.apache.ignite.cache.CacheMode;
 public class IgniteConfig {
 
     @Bean(destroyMethod = "close")
+    @SuppressWarnings("unchecked")
     public Ignite igniteInstance() {
-        IgniteConfiguration cfg = new IgniteConfiguration();
+        IgniteConfiguration igniteConfiguration = new IgniteConfiguration();
         // Setting some custom name for the node.
-        cfg.setIgniteInstanceName("springDataNode");
+        igniteConfiguration.setIgniteInstanceName("springDataNode");
         // Enabling peer-class loading feature.
-        cfg.setPeerClassLoadingEnabled(true);
-        
-     
-       
+        igniteConfiguration.setPeerClassLoadingEnabled(true);
+
         // Defining and creating a new cache to be used by Ignite Spring Data
         // repository.
-        CacheConfiguration ccfg = new CacheConfiguration("FlightCache");
-        ccfg.setAtomicityMode(TRANSACTIONAL);
-        
-         ccfg.setCacheMode(CacheMode.REPLICATED);
+        CacheConfiguration cacheConfiguration = new CacheConfiguration("FlightCache");
+        cacheConfiguration.setAtomicityMode(TRANSACTIONAL);
+        //cacheConfiguration.setCacheStoreFactory(new FactoryBuilder.SingletonFactory<CacheStore>());
+        cacheConfiguration.setCacheMode(CacheMode.REPLICATED);
 
+        // cacheConfiguration.setCacheStoreFactory(FactoryBuilder.factoryOf(FlightRepository.class));
+        //cacheConfiguration.setReadThrough(true);
+       // cacheConfiguration.setWriteThrough(true);
+        cacheConfiguration.setWriteBehindEnabled(true);
         // Setting SQL schema for the cache.
-        ccfg.setIndexedTypes(Long.class, Flight.class);          
-        cfg.setActiveOnStart(true);    
-        cfg.setCacheConfiguration(ccfg);       
-        return Ignition.start(cfg);
+        cacheConfiguration.setIndexedTypes(Long.class, Flight.class);
+        igniteConfiguration.setActiveOnStart(true);
+
+        igniteConfiguration.setCacheConfiguration(cacheConfiguration);
+        SpringCacheManager springCacheManager = new SpringCacheManager();
+        springCacheManager.setConfiguration(igniteConfiguration);
+        //Ignite ignite = Ignition.start("example-ignite.xml");
+        // IgniteCache<Long, Flight> cache = ignite.getOrCreateCache(cacheConfiguration);
+        return Ignition.start(igniteConfiguration);
+
+        //return ignite;
     }
-    
+
     // https://github.com/apache/ignite/blob/master/examples/src/main/java/org/apache/ignite/examples/datagrid/CacheQueryExample.java
 }
